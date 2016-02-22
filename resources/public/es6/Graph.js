ae1b8811a234a418b4160bba410334a52fc03df6
Alex Petrov <oleksandr.petrov@gmail.com>
2016-02-22 20:56:25 +0100

    Rough sketch of cubism dash

--- resources/public/es6/Graph.js
+++ resources/public/es6/Graph.js
@@ -1,6 +1,105 @@
 import React from 'react';
 import d3 from 'd3';
-import MG from 'metrics-graphics';
+import cubism from 'cubism';
+
+// Graphite understands seconds since UNIX epoch.
+function cubism_graphiteFormatDate(time) {
+  return Math.floor(time / 1000);
+}
+
+
+cubism.metric.prototype.extent = function() {
+  var i = 0,
+      n = this.context.size(),
+      value,
+      min = Infinity,
+      max = -Infinity;
+  while (++i < n) {
+    value = this.valueAt(i);
+    if (value < min) min = value;
+    if (value > max) max = value;
+  }
+  if (window.location.hash == "#wtf") {
+    return [0, max * 5];
+  } else {
+    return [0, max];
+  }
+};
+
+cubism.context.prototype.graphite_json = function(host) {
+  if (!arguments.length) host = "";
+  var source = {},
+      context = this;
+
+  source.metric = function(expression) {
+    var sum = "sum";
+    var firstRun = true;
+    var start_ = null;
+
+    var metric = context.metric(function(start, stop, step, callback) {
+      var target = expression;
+
+      // Apply the summarize, if necessary.
+      /* if (step !== 1e4) target = "summarize(" + target + ",'"
+         + (!(step % 36e5) ? step / 36e5 + "hour" : !(step % 6e4) ? step / 6e4 + "min" : step / 1e3 + "sec")
+         + "','" + sum + "')"; */
+
+      d3.json(host + "/render?format=json"
+          + "&target=" + encodeURIComponent(target)
+          + "&from=" + (start_ || "-24min") //|| cubism_graphiteFormatDate(start)  //"-24min" // off-by-two?
+          , function(text) {
+            if (!text) {
+              return callback(new Error("unable to load data"));
+            } else {
+              var s = text[0].datapoints;
+
+              var dp = s.map((a) => a[0]);
+
+              var res = [];
+              if (firstRun) {
+                for (var i = 0; i < 1440 - dp.length; i++) {
+                  res.push(0);
+                }
+                firstRun = false;
+              }
+
+              //console.log(dp.length)
+              if (dp.length > 0) {
+                start_ = s[s.length - 1][1] + 1;
+              }
+              var downstream = res.concat(dp);
+              console.log(downstream);
+              callback(null, downstream);
+            }
+
+      });
+    }, expression += "");
+
+    metric.summarize = function(_) {
+      sum = _;
+      return metric;
+    };
+
+    return metric;
+  };
+
+  source.find = function(pattern, callback) {
+    d3.json(host + "/metrics/find?format=completer"
+        + "&query=" + encodeURIComponent(pattern), function(result) {
+      if (!result) return callback(new Error("unable to find metrics"));
+      callback(null, result.metrics.map(function(d) { return d.path; }));
+    });
+  };
+
+  // Returns the graphite host.
+  source.toString = function() {
+    return host;
+  };
+
+  return source;
+};
+
+
 
 const Graph = React.createClass({
   getInitialState: function() {
@@ -10,53 +109,56 @@ const Graph = React.createClass({
   },
 
   componentDidMount: function() {
-    this.tick(this);
-  },
+    var context = cubism.context().step(10000).serverDelay(5000).size(1440), // a default context
+        graphite = context.graphite_json("http://localhost:8484"),
+        comparison = context.comparison()
 
-  tick: function() {
-    var targets = this.props.metrics.map((m) => `target=${m}`).join("&");
+    d3.select("body")
+      .selectAll(".axis")
+      .data(["top", "bottom"])
+      .enter()
+      .append("div")
+      .attr("class", function(d) { return d + " axis"; })
+      .each(function(d) { d3.select(this).call(context.axis().ticks(12).orient(d)); });
 
-    this.serverRequest = d3.json(`/render?${targets}&from=${this.props.from}&until=${this.props.until}&format=json&maxDataPoints=956`, (result) => {
-      this.setState({data: result});
+    d3.select("body").append("div")
+      .attr("class", "rule")
+      .call(context.rule());
+
+    var horizon = context.horizon()
+                         .height(50);
+
+    this.props.metrics.map((metric) => {
+      d3.select("body").selectAll(metric)
+        .data([graphite.metric(metric)])
+        .enter()
+        .insert("div", ".bottom")
+        .attr("class", "horizon")
+        .attr("id",  metric)
+        .call(horizon);
     });
-    setTimeout(this.tick, 1000);
-  },
+    /*
+       d3.select("body").selectAll(".comparison")
+       .data([["internal.cyanite.jvm.memory.total.used",
+       "internal.cyanite.jvm.memory.heap.used"].map(graphite.metric)])
+       .enter()
+       .insert("div", ".bottom")
+       .attr("class", "horizon")
+       .call(comparison);
+     */
 
-  componentDidUpdate: function() {
-    console.log("updating")
-    var data = this.state.data.map((ds) => {
-      return ds.datapoints.map((a) => {
-        return { y: a[0],
-                 x: new Date(a[1] * 1000) };
-      });
+    context.on("focus", function(i) {
+      d3.selectAll(".value").style("right", i == null ? null : context.size() - i + "px");
     });
 
-    MG.data_graphic({
-      title: this.props.metric,
-      description: ".",
-      width: 800,
-      height: 300,
-      right: 40,
-      data: data,
-      area: false,
-      show_tooltips: false,
-      target: '#asd',
-      x_accessor: 'x',
-      y_accessor: 'y'
-    });
+//      .data(graphite.metric("internal.cyanite.jvm.memory.heap.used"))
+//      .call(context.horizon());
 
-  },
-
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
   },
 
   render: function() {
     return (
-      <div className='graph_container'>
-        <div id='asd'>
-
-        </div>
+      <div className='h'>
       </div>
     );
   }
--- resources/public/es6/main.js
+++ resources/public/es6/main.js
@@ -5,7 +5,12 @@ import Graph from './Graph';
 
 ReactDOM.render(
   <div id='container'>
-    <Graph metrics={["internal.cyanite.jvm.memory.heap.used", "internal.cyanite.jvm.memory.heap.committed", "internal.cyanite.jvm.memory.total.used"]}
+    <Graph metrics={["internal.cyanite.jvm.memory.total.used",
+                     "internal.cyanite.jvm.memory.heap.used",
+                     "internal.cyanite.jvm.memory.heap.committed",
+                     "internal.cyanite.jvm.thread.count",
+                     "internal.cyanite.jvm.thread.runnable.count",
+      "a.b"]}
       from="-5min"
       until="now"
       maxDataPoints="956"
